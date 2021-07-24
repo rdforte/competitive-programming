e9fbfb923f5230898c00fd11002914e532e76b9f
Ryan Forte <forte.ryan@hotmail.com>
2021-07-24 17:20:47 +1000

    FLOW006

---
+++ codeChef/Beginner/FLOW006/index.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+    int a;
+    std::cin >> a;
+    int n[a];
+
+    for (int i = 0; i < a; i++)
+        std::cin >> n[i];
+
+    for (int i = 0; i < a; i++)
+    {
+        int currentNumber = n[i];
+        int sum = 0;
+        while (currentNumber / 10 >= 1)
+        {
+            int furthestRightNumber = currentNumber % 10;
+            sum += furthestRightNumber;
+            currentNumber -= furthestRightNumber;
+            currentNumber /= 10;
+        }
+        std::cout << (sum + currentNumber) << std::endl;
+    }
+}
