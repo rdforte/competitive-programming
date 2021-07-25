b20a6d0870c5775028604babd340efd11eeca484
Ryan Forte <forte.ryan@hotmail.com>
2021-07-25 12:11:33 +1000

    refactor FLOW006

--- codeChef/Beginner/FLOW006/index.cpp
+++ codeChef/Beginner/FLOW006/index.cpp
@@ -4,14 +4,15 @@ int main()
 {
     int a;
     std::cin >> a;
-    int n[a];
+    // int n[a];
 
-    for (int i = 0; i < a; i++)
-        std::cin >> n[i];
+    // for (int i = 0; i < a; i++)
+    //     std::cin >> n[i];
 
     for (int i = 0; i < a; i++)
     {
-        int currentNumber = n[i];
+        int currentNumber;
+        std::cin >> currentNumber;
         int sum = 0;
         while (currentNumber / 10 >= 1)
         {
