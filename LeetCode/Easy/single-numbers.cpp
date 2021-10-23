50aa8a83eb082b0dcb87b1785d57a307ed5b9446
Ryan Forte <forte.ryan@hotmail.com>
2021-10-23 14:21:45 +1100

    single-numbers.cpp

--- LeetCode/Easy/single-numbers.cpp
+++ LeetCode/Easy/single-numbers.cpp
@@ -1,37 +1,48 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <stdexcept>
 
 class Solution
 {
 public:
-    int singleNumber(std::vector<int> &nums)
+    // Hash Map
+    // time complexity = O(n) we have to complete a max of 2 loops of n operations. The biggest loop is most likely to be the first
+    // where we add the items to the hashmap.
+    // space complexity O(n) we structure a map to keep track of the items
+    int singleNumberV1(std::vector<int> &nums)
     {
         std::map<int, int> m;
         for (int i = 0; i < nums.size(); i++)
+            m[nums[i]]++;
+
+        for (auto const [key, value] : m)
         {
-            if (m[nums[i]] == 0)
-            {
-                m[nums[i]] = 1;
-            }
-            else
-            {
-                m[nums[i]] = 1;
-            }
-        };
-        for (auto element : m)
-        {
+            if (value == 1)
+                return key;
         }
+
+        throw std::invalid_argument("There was no single int");
+    }
+    // XOR method
+    // XOR of 0 and bit is always the bit
+    // (x ^ x) always cancels out
+    // therefore we can be left with the number that is different
+    // time complexity O(n)
+    // space complexity O(1) we only keep track of the current xor
+    int singleNumberV2(std::vector<int> &nums)
+    {
+        int n = 0;
+        for (int num : nums)
+        {
+            n ^= num;
+        }
+        return n;
     }
 };
 
 int main()
 {
-    // std::map<int, int> m;
-    // std::cout << (m[1] == 0);
-    int data[] = {1, 2, 3, 4, 5, 6};
-
-    for (int n : data)
-    {
-    }
+    std::vector<int> v{1, 2, 2, 1, 4};
+    std::cout << Solution().singleNumberV2(v);
 }
