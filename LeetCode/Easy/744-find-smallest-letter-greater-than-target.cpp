e3aad16eb627798cecebf5c08dd8af09d6e566ad
Ryan Forte <forte.ryan@hotmail.com>
2022-02-27 12:36:35 +1100

    LeetCode 744 smallest letter greater than target

---
+++ LeetCode/Easy/744-find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution
+{
+public:
+  // You can compare characters using comparison operators.
+  // Spacetime = O(1) no extra space needed.
+  // Runtime = O(n) have to loop through all items if target is last item in array.
+  char nextGreatestLetter(std::vector<char> &letters, char target)
+  {
+    for (int i = 0; i < letters.size(); i++)
+    {
+      if (letters[i] > target)
+        return letters[i];
+    }
+    return letters[0];
+  };
+};
+
+int main()
+{
+  std::vector<char> v{'a', 'b', 'c', 'd'};
+
+  char sol = Solution().nextGreatestLetter(v, 'd');
+  std::cout << sol;
+}
