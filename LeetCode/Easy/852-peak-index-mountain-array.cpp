15d4476d6cda48eb757dc7783afe06a8aea614e3
Ryan Forte <forte.ryan@hotmail.com>
2022-02-27 14:59:44 +1100

    852. Peak Index in a Mountain Array

---
+++ LeetCode/Easy/852-peak-index-mountain-array.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+  // Recursive approach
+  // Spacetime O(1) we just have to keep track of the middle index
+  // Runtime O(log.n) we halve the array every single time.
+  int peakIndexInMountainArray(std::vector<int> &arr)
+  {
+    return peakRecursion(arr, 0, arr.size() - 1);
+  }
+  int peakRecursion(std::vector<int> &arr, int lower, int upper)
+  {
+    if (upper == lower)
+      return upper;
+
+    int middle = lower + ((upper - lower) / 2);
+
+    if (arr[middle] < arr[middle + 1])
+    {
+      return peakRecursion(arr, middle + 1, upper);
+    }
+    else
+    {
+      return peakRecursion(arr, lower, middle);
+    }
+  }
+};
+
+int main()
+{
+  std::vector<int> mount{0, 1, 2, 3, 4, 5};
+
+  int res = Solution().peakIndexInMountainArray(mount);
+  std::cout << res;
+}
