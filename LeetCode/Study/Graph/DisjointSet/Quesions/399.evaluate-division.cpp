8f9ca82976ca43a7e539d151cdd8ea2b6b67c65f
Ryan Forte <forte.ryan@hotmail.com>
2022-05-18 08:35:47 +1000

    q 399 solution

--- LeetCode/Study/Graph/DisjointSet/Quesions/399.evaluate-division.cpp
+++ LeetCode/Study/Graph/DisjointSet/Quesions/399.evaluate-division.cpp
@@ -7,146 +7,156 @@
 
 using namespace std;
 
-// Node represents the Node in the graph with its current weight and a link to its parent.
-class Node
+struct Node
 {
-public:
   string parent;
   double weight;
 
-  Node(string n, double w)
-  {
-    parent = n;
-    weight = w;
-  }
+  Node() : parent(""), weight(1) {}
+  Node(string p) : parent(p), weight(1) {}
+  Node(string p, double w) : parent(p), weight(w) {}
 };
 
-class UnionFind
-{
-private:
-  unordered_map<string, Node *> roots;
-
-public:
-  UnionFind(set<string> nodes)
-  {
-    for (auto n : nodes)
-    {
-      Node *node = new Node(n, 1);
-      roots[n] = node;
-    }
-  }
-
-  string findRoot(string n)
-  {
-    while (roots[n]->parent != n)
-    {
-      n = roots[n]->parent;
-    }
-    return n;
-  }
-
-  void unionNodes(string a, string b, double result)
-  {
-    string rootA = findRoot(a);
-    string rootB = findRoot(b);
-
-    if (rootA != rootB)
-    {
-
-      double bResWeight = roots[a]->weight / result;
-      double ratio = bResWeight / roots[b]->weight;
-      roots[b]->weight = bResWeight;
-
-      for (auto r : roots)
-      {
-        if (r.second->parent == rootB && r.first != b)
-        {
-          r.second->weight *= ratio;
-        }
-      }
-      cout << a << " ---- " << b << "-------------------------\n";
-      for (auto r : roots)
-      {
-        cout << r.first << " : " << r.second->parent << " == " << r.second->weight << "\n";
-      }
-      cout << "--------------------------------\n";
-
-      roots[rootB]->parent = rootA;
-    }
-  }
-
-  bool
-  isConnected(string a, string b)
-  {
-    if (roots.find(a) != roots.end() &&
-        roots.find(b) != roots.end())
-    {
-      return findRoot(a) == findRoot(b);
-    }
-
-    return false;
-  }
-
-  unordered_map<string, Node *> getRoots()
-  {
-    return roots;
-  }
-};
-
+// Apprach Union Find With Weights
 class Solution
 {
+private:
+  unordered_map<string, Node> graphIdWeight;
+
 public:
+  Solution()
+  {
+    graphIdWeight = {};
+  }
+
   vector<double> calcEquation(vector<vector<string>> &equations, vector<double> &values, vector<vector<string>> &queries)
   {
-    set<string> s{};
-    for (auto eq : equations)
-    {
-      s.insert(eq[0]);
-      s.insert(eq[1]);
-    }
 
-    UnionFind uf(s);
-
+    // Build Union Groups
     for (int i = 0; i < equations.size(); i++)
     {
-      uf.unionNodes(equations[i][0], equations[i][1], values[i]);
+      vector<string> equation = equations[i];
+      string dividend = equation[0];
+      string divisor = equation[1];
+      double quotient = values[i];
+
+      // call union
+      unionise(dividend, divisor, quotient);
     }
 
+    // run through the evaluation with 'lazy' updates in the find function.
     vector<double> results{};
-    for (auto q : queries)
+
+    for (int i = 0; i < queries.size(); i++)
     {
-      bool isConnected = uf.isConnected(q[0], q[1]);
-      if (isConnected)
+      string dividend = queries[i][0], divisor = queries[i][1];
+
+      if (graphIdWeight.find(dividend) == graphIdWeight.end() || graphIdWeight.find(divisor) == graphIdWeight.end())
       {
-        double val = uf.getRoots()[q[0]]->weight / uf.getRoots()[q[1]]->weight;
-        results.push_back(val);
+        results.push_back(-1);
       }
       else
       {
-        results.push_back(-1.0);
+        Node dividendEntry = find(dividend);
+        Node divisorEntry = find(divisor);
+
+        string dividendGraphId = dividendEntry.parent;
+        string divisorGraphId = divisorEntry.parent;
+        double dividendWeight = dividendEntry.weight;
+        double divisorWeight = divisorEntry.weight;
+
+        if (dividendGraphId != divisorGraphId)
+        {
+          // not part of the same group
+          results.push_back(-1);
+        }
+        else
+        {
+          results.push_back(dividendWeight / divisorWeight);
+        }
       }
     }
 
+    for (auto gi : graphIdWeight)
+    {
+      cout << gi.first << " : " << gi.second.parent << " == " << gi.second.weight << endl;
+    }
+
     return results;
+  }
+
+private:
+  Node find(string nodeId)
+  {
+    // If there is no node yet then create it.
+    if (graphIdWeight.find(nodeId) == graphIdWeight.end())
+    {
+      graphIdWeight[nodeId] = Node(nodeId);
+    }
+
+    Node entry = graphIdWeight[nodeId];
+
+    if (entry.parent != nodeId)
+    {
+      // recursively get the root
+      Node newEntry = find(entry.parent);
+      graphIdWeight[nodeId] = Node(newEntry.parent, entry.weight * newEntry.weight);
+    }
+
+    return graphIdWeight[nodeId];
+  }
+
+  void unionise(string dividend, string divisor, double value)
+  {
+    Node dividendEntry = find(dividend);
+    Node divisorEntry = find(divisor);
+
+    string dividendGraphId = dividendEntry.parent;
+    string divisorGraphId = divisorEntry.parent;
+    double dividendWeight = dividendEntry.weight;
+    double divisorWeight = divisorEntry.weight;
+
+    // merge the two groups together
+    if (dividendGraphId != divisorGraphId)
+    {
+      graphIdWeight[dividendGraphId] = Node(divisorGraphId, (divisorWeight * value) / dividendWeight);
+    }
   }
 };
 
 int main()
 {
 
-  // expected: [1.33333,1.00000,-1.00000]
+  // expected: [1.33333, 1.00000, -1.00000]
+  vector<vector<string>> equations{
+      {"a", "e"},
+      {"b", "e"},
+  };
+
+  vector<double> values{
+      4.0, 3.0};
+
+  vector<vector<string>> queries{
+      {"a", "b"},
+      {"e", "e"},
+      {"x", "x"},
+  };
+
+  // expected: [3.75000,0.40000,5.00000,0.20000]
   // vector<vector<string>> equations{
-  //     {"a", "e"},
-  //     {"b", "e"},
+  //     {"a", "b"},
+  //     {"b", "c"},
+  //     {"bc", "cd"},
   // };
 
   // vector<double> values{
-  //     4.0, 3.0};
+  //     1.5, 2.5, 5.0};
 
   // vector<vector<string>> queries{
-  //     {"a", "b"},
-  //     {"e", "e"},
-  //     {"x", "x"},
+  //     {"a", "c"},
+  //     {"c", "b"},
+  //     {"bc", "cd"},
+  //     {"cd", "bc"},
   // };
 
   // expected: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
@@ -167,27 +177,27 @@ int main()
   // };
 
   // expected: [1.13333,16.80000,1.50000,1.00000,0.05952,2.26667,0.44118,-1.00000,-1.00000]
-  vector<vector<string>> equations{
-      {"x1", "x2"},
-      {"x2", "x3"},
-      {"x1", "x4"},
-      {"x2", "x5"},
-  };
+  // vector<vector<string>> equations{
+  //     {"x1", "x2"},
+  //     {"x2", "x3"},
+  //     {"x1", "x4"},
+  //     {"x2", "x5"},
+  // };
 
-  vector<double> values{
-      3.0, 0.5, 3.4, 5.6};
+  // vector<double> values{
+  //     3.0, 0.5, 3.4, 5.6};
 
-  vector<vector<string>> queries{
-      {"x2", "x4"},
-      {"x1", "x5"},
-      {"x1", "x3"},
-      {"x5", "x5"},
-      {"x5", "x1"},
-      {"x3", "x4"},
-      {"x4", "x3"},
-      {"x6", "x6"},
-      {"x0", "x0"},
-  };
+  // vector<vector<string>> queries{
+  //     {"x2", "x4"},
+  //     {"x1", "x5"},
+  //     {"x1", "x3"},
+  //     {"x5", "x5"},
+  //     {"x5", "x1"},
+  //     {"x3", "x4"},
+  //     {"x4", "x3"},
+  //     {"x6", "x6"},
+  //     {"x0", "x0"},
+  // };
 
   vector<double> sol = Solution().calcEquation(equations, values, queries);
 
