5bfdababf4066920b7d623588b2a2ddaaf40e2df
Ryan Forte <forte.ryan@hotmail.com>
2022-06-22 08:29:34 +1000

    BFS

---
+++ LeetCode/Study/Graph/BFS/questions/find-if-path-exists-in-graph.cpp
@@ -0,0 +1,58 @@
+// https://leetcode.com/explore/learn/card/graph/620/breadth-first-search-in-graph/3894/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution
+{
+public:
+  bool validPath(int n, vector<vector<int>> &edges, int source, int destination)
+  {
+    vector<vector<int>> adjacencyList(n);
+    vector<bool> visited(n);
+
+    for (auto e : edges)
+    {
+      adjacencyList[e[0]].push_back(e[1]);
+      adjacencyList[e[1]].push_back(e[0]);
+    }
+
+    queue<int> q;
+
+    q.push(source);
+
+    while (!q.empty())
+    {
+      int node = q.front();
+      q.pop();
+
+      if (node == destination)
+        return true;
+
+      for (auto n : adjacencyList[node])
+      {
+        if (!visited[n])
+        {
+          visited[n] = true;
+          q.push(n);
+        }
+      }
+    }
+    return false;
+  }
+};
+
+int main()
+{
+  cout << boolalpha;
+  vector<vector<int>> edges{
+      {0, 1},
+      {1, 2},
+      {2, 0},
+  };
+
+  cout << Solution().validPath(3, edges, 0, 2);
+}
---
+++ LeetCode/Study/Graph/MinSpanningTree/Notes.md
@@ -0,0 +1,10 @@
+# Spanning Tree
+A **Spanning Tree** is a connected subgraph in an undirected graph where
+all vertices are connected with the minimum number of edges.
+
+all pink edges [(A, B), (A, C), (A, D), (A, E)] form a tree, which is a spanning tree of this undirected graph. Note that [(A, E), (A, B), (B, C), (C, D)] is also a spanning tree of the undirected graph. Thus, an “undirected graph” can have multiple spanning trees.
+
+![spanning tree](./assets/spanning-tree.png)
+
+A **Minimum Spanning Tree** is a spanning tree with the minimum possible total edge weight in a **weighted undirected graph**.
+
