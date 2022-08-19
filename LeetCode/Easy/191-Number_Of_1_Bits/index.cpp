f9f0d8facaa3ab3455453f015f7f642588b9c92b
Ryan Forte <forte.ryan@hotmail.com>
2022-08-20 08:50:13 +1000

    leetcode 191 number of 1 bits

---
+++ LeetCode/Easy/191-Number_Of_1_Bits/index.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int hammingWeight(uint32_t n)
+  {
+    int numBits = 0;
+    u_int mask = 1;
+
+    for (int i = 0; i < 32; i++)
+    {
+      cout << "mask: " << mask << "\n";
+      if ((n & mask) != 0)
+        numBits++;
+      mask <<= 1;
+    }
+
+    return numBits;
+  }
+};
+
+int main()
+{
+  uint32_t n = 00000000000000000000000000001011; // is the same as 11
+
+  // We are using actual numbers though when we use bitwise operators it
+  // treats the numbers as bits.
+  // the mask starts at 1 and then increments to 2,4,8,16,32 ...
+  // though in binary this looks like, 1, 10, 100, 1000, 10000
+  // so we can use this as a mask.
+
+  // cout << Solution().hammingWeight(11); // 3
+  cout << (11 & 8);
+}
---
+++ LeetCode/Easy/338-Counting_Bits/index.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  vector<int> countBits(int n)
+  {
+    vector<int> numBits{};
+
+    for (int i = 0; i <= n; i++)
+    {
+      int bitCount = 0;
+      int num = i;
+
+      while (num > 0)
+      {
+      }
+    }
+  }
+};
