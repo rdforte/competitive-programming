c10159fd99890eb629c590ebe08f2720ecc49377
Ryan Forte <forte.ryan@hotmail.com>
2022-11-06 09:35:40 +1100

    cses missing number

---
+++ CSES/IntroductoryProblems/missingNumber/index.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  int missing = n;
+
+  for (int i = 1; i < n; i++)
+  {
+    int input;
+    cin >> input;
+    missing ^= input ^ i;
+  }
+
+  cout << missing;
+}
