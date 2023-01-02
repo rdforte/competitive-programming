004e8d6bd74a15cc4b02a4e5c4daa7d6034bbe7b
Ryan Forte <forte.ryan@hotmail.com>
2023-01-03 08:50:33 +1100

    leetcode 409 longest palindrome

---
+++ LeetCode/Easy/409-longest-palindrome/index.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int longestPalindrome(string s)
+  {
+    unordered_map<char, int> m;
+    for (int i = 0; i < s.size(); i++)
+    {
+      m[s[i]]++;
+    }
+
+    int count = 0;
+
+    for (auto c : m)
+    {
+      count += (c.second / 2 * 2);
+      if ((count & 1) == 0 and (c.second & 1) == 1)
+        count++;
+    }
+
+    return count;
+  }
+};
+
+int main()
+{
+  cout << Solution().longestPalindrome("ccc");
+}
