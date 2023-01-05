e646e90d0a7cb6df95a9e2b33635f494f8bcefbf
Ryan Forte <forte.ryan@hotmail.com>
2023-01-05 14:39:52 +1100

    updated comment

--- random/vectors/unique.cpp
+++ random/vectors/unique.cpp
@@ -7,7 +7,7 @@ int main()
   // elements need to be in order for unique to work
   vector<int> v = {1, 2, 2, 2, 3, 4, 4, 5};
 
-  // points to the last element which was not removed.
+  // points to the last element that follows the element which was not removed.
   auto it = unique(v.begin(), v.end());
 
   v.erase(it, v.end()); // need to resize the array
