f9c169b278ee3d348c0fd65e2d13cb95691d6c17
Ryan Forte <forte.ryan@hotmail.com>
2023-01-05 14:38:08 +1100

    random using unique cpp vector method

---
+++ random/vectors/unique.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  // elements need to be in order for unique to work
+  vector<int> v = {1, 2, 2, 2, 3, 4, 4, 5};
+
+  // points to the last element which was not removed.
+  auto it = unique(v.begin(), v.end());
+
+  v.erase(it, v.end()); // need to resize the array
+
+  for (auto i : v)
+  {
+    cout << i << ", ";
+  }
+}
