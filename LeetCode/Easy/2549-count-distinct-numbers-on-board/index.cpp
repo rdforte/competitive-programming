f84add92e89ea4993648cf9d87739100c8d18725
Ryan Forte <forte.ryan@hotmail.com>
2023-01-31 08:16:20 +1100

    leetcode comp 330

---
+++ LeetCode/Easy/2549-count-distinct-numbers-on-board/index.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/count-distinct-numbers-on-board/description/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int distinctIntegers(int n)
+  {
+    set<int> s;
+    s.insert(n);
+
+    while (n > 1)
+    {
+      int max = 0;
+      for (int i = 1; i <= n; i++)
+      {
+        if (n % i == 1)
+        {
+          s.insert(i);
+          max = i;
+        }
+      }
+      n = max;
+    }
+    return s.size();
+  }
+};
+
+int main()
+{
+  cout << Solution().distinctIntegers(1);
+}
