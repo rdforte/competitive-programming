d1fa914febb2c836f25d6c692f5216e03ca9ca7a
Ryan Forte <forte.ryan@hotmail.com>
2023-08-12 09:28:35 +1000

    leetcode 98 refactor valid bst

--- LeetCode/Medium/98-validate-binary-search-tree/js/validBST.js
+++ LeetCode/Medium/98-validate-binary-search-tree/js/validBST.js
@@ -1,9 +1,18 @@
-module.exports.isValidBST = (root) => {
-  return validateBSTRecursively(
-    root,
-    Number.NEGATIVE_INFINITY,
-    Number.POSITIVE_INFINITY
-  );
+module.exports.isValidBST = (root) => dfsBST(root);
+
+const dfsBST = (
+  node,
+  min = Number.NEGATIVE_INFINITY,
+  max = Number.POSITIVE_INFINITY
+) => {
+  if (node == null) return true;
+
+  if (!(node.val > min && node.val < max)) return false;
+
+  const leftIsValid = dfsBST(node.left, min, node.val);
+  const rightIsValid = dfsBST(node.right, node.val, max);
+
+  return leftIsValid && rightIsValid;
 };
 
 module.exports.createNode = (val, left, right) => new Node(val, left, right);
@@ -19,22 +28,3 @@ class Node {
     this.right = right;
   }
 }
-// min = 6
-// max = Infinity
-const validateBSTRecursively = (node, min, max) => {
-  if (node == null) return true;
-
-  let isValid = node.val > min && node.val < max;
-
-  if (!isValid) return false;
-
-  if (!validateBSTRecursively(node.left, min, node.val)) {
-    isValid = false;
-  }
-
-  if (!validateBSTRecursively(node.right, node.val, max)) {
-    isValid = false;
-  }
-
-  return isValid;
-};
