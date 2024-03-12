98f9f76a6d5592f5345fca5be38413370391412e
rf <rf@ryans-MacBook-Air.local>
2024-03-13 09:30:14 +1100

    leetcode 2402 meeting rooms 3 [temp]

---
+++ LeetCode/Hard/2402-meeting-rooms-3/go/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	meetings := [][]int{
+		{0, 10},
+		{1, 5},
+		{2, 7},
+		{3, 4},
+	}
+	fmt.Println(mostBooked(2, meetings))
+}
+
+func mostBooked(n int, meetings [][]int) int {
+	// maxMeetingEnd := 0
+	sort.Slice(meetings, func(i, j int) bool {
+		// maxMeetingEnd = int(math.Max(float64(meetings[i][1]), float64(maxMeetingEnd)))
+		// maxMeetingEnd = int(math.Max(float64(meetings[j][1]), float64(maxMeetingEnd)))
+		return meetings[i][0] < meetings[j][0]
+	})
+
+	roomsEndTime := make([]int, n)
+	roomsOccupied := make([]bool, n)
+	roomsCount := make([]int, n)
+
+	curTime := 0
+TIMES:
+	for len(meetings) > 0 {
+		for o, roomOccupied := range roomsOccupied {
+			if len(meetings) == 0 {
+				break TIMES
+			}
+
+			if roomOccupied && curTime >= roomsEndTime[o] {
+				roomsEndTime[o] = 0
+				roomOccupied = false
+			}
+
+			if !roomOccupied && meetings[0][0] <= curTime {
+				overlap := (curTime - meetings[0][0])
+				if overlap < 0 {
+					overlap = 0
+				}
+
+				roomsEndTime[o] = meetings[0][1] + overlap
+				roomsOccupied[o] = true
+				roomsCount[o]++
+				meetings = meetings[1:]
+			}
+		}
+
+		curTime++
+	}
+
+	roomNum := 0
+	count := roomsCount[0]
+	for i, c := range roomsCount {
+		if c > count {
+			roomNum = i
+		}
+	}
+
+	return roomNum
+}
