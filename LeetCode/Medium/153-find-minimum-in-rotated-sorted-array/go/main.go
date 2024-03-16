3f4611dbdd7a969511d28386fffdbbd50316767e
rf <rf@ryans-MacBook-Air.local>
2024-03-17 08:44:12 +1100

    add notes for binary search shifted array

--- LeetCode/Medium/153-find-minimum-in-rotated-sorted-array/go/main.go
+++ LeetCode/Medium/153-find-minimum-in-rotated-sorted-array/go/main.go
@@ -9,16 +9,19 @@ func main() {
 	fmt.Println(findMin([]int{3, 1, 2}))
 }
 
+// One thing to note here is that when we do nums[r] < mid as in is right less than mid then we know that mid cant be our answer so we can set left to mid +1.
+// but that also means if we shift our right pointer to mid mid can be inclusive of the answer we are looking for so we don't do r = mid -1 and just r = mid
+
 func findMin(nums []int) int {
 	l, r := 0, len(nums)-1
 	for l < r {
 		midI := l + (r-l)/2
 		mid := nums[midI]
 
-		if nums[r] > mid {
-			r = midI
-		} else {
+		if nums[r] < mid {
 			l = midI + 1
+		} else {
+			r = midI
 		}
 	}
 	return nums[l]
