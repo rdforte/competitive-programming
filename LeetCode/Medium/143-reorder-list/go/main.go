80a28f69085710270dd4e3c647e7b30c40b34fc7
rf <rf@ryans-MacBook-Air.local>
2024-03-22 08:32:41 +1100

    leetcode 143-reorder-list

---
+++ LeetCode/Medium/143-reorder-list/go/main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	five := &ListNode{5, nil}
+	four := &ListNode{4, five}
+	three := &ListNode{3, four}
+	two := &ListNode{2, three}
+	head := &ListNode{1, two}
+
+	reorderList(head)
+	fmt.Println(head)
+}
+
+func reorderList(head *ListNode) {
+	mid := head
+	fast := head
+
+	for fast != nil && fast.Next != nil {
+		mid = mid.Next
+		fast = fast.Next.Next
+	}
+
+	// reverse mid
+	var prev *ListNode
+	next := mid
+	for next != nil {
+		cur := next
+		next = cur.Next
+		cur.Next = prev
+		prev = cur
+	}
+
+	primary := head
+	secondary := prev
+
+	for secondary.Next != nil {
+		tmp := primary.Next
+		primary.Next = secondary
+		secondary = secondary.Next
+		primary.Next.Next = tmp
+		primary = tmp
+	}
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
