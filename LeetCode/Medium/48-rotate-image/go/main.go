386bec239863255377747fa1525fa110084e973a
rf <rf@ryans-MacBook-Air.local>
2024-03-28 07:42:50 +1100

    leetcode 40 rotate image

--- LeetCode/Medium/48-rotate-image/go/main.go
+++ LeetCode/Medium/48-rotate-image/go/main.go
@@ -14,66 +14,24 @@ func main() {
 }
 
 func rotate(matrix [][]int) {
-	rotateCorners(matrix)
+	t, r, b, l := 0, len(matrix[0])-1, len(matrix)-1, 0
 
-	// rotate top to right side
-	offset := 1
+	for l < r {
+		for i := 0; i < r-l; i++ {
+			tr := matrix[t+i][r]
+			matrix[t+i][r] = matrix[t][l+i]
 
-	queue := []int{}
-	for i := offset; i < len(matrix)-offset; i++ {
-		queue = append(queue, matrix[0][i])
-	}
+			br := matrix[b][r-i]
+			matrix[b][r-i] = tr
 
-	for i := offset; i < len(matrix)-offset; i++ {
-		queue = append(queue, matrix[i][len(matrix)-1])
-	}
+			bl := matrix[b-i][l]
+			matrix[b-i][l] = br
 
-	for c := offset; c < len(matrix)-offset; c++ {
-		matrix[c][len(matrix)-1] = queue[0]
-		queue = queue[1:]
-	}
-
-	// move right side to bottom
-	for i := len(matrix) - 1 - offset; i >= offset; i-- {
-		queue = append(queue, matrix[len(matrix)-1][i])
-	}
-
-	for r := len(matrix) - 1 - offset; r >= offset; r-- {
-		matrix[len(matrix)-1][r] = queue[0]
-		queue = queue[1:]
-	}
-
-	fmt.Println(queue)
-}
-
-func rotateCorners(matrix [][]int) {
-	topLeft := []int{0, 0}
-	topRight := []int{0, len(matrix[0]) - 1}
-	bottomRight := []int{len(matrix) - 1, len(matrix[len(matrix)-1]) - 1}
-	bottomLeft := []int{len(matrix) - 1, 0}
-
-	for topLeft[0] < bottomRight[0] {
-		oldTopRight := matrix[topRight[0]][topRight[1]]
-		matrix[topRight[0]][topRight[1]] = matrix[topLeft[0]][topLeft[1]]
-
-		oldBottomRight := matrix[bottomRight[0]][bottomRight[1]]
-		matrix[bottomRight[0]][bottomRight[1]] = oldTopRight
-
-		oldBottomLeft := matrix[bottomLeft[0]][bottomLeft[1]]
-		matrix[bottomLeft[0]][bottomLeft[1]] = oldBottomRight
-
-		matrix[topLeft[0]][topLeft[1]] = oldBottomLeft
-
-		topLeft[0]++
-		topLeft[1]++
-
-		topRight[0]++
-		topRight[1]--
-
-		bottomRight[0]--
-		bottomRight[1]--
-
-		bottomLeft[0]--
-		bottomLeft[1]++
+			matrix[t][l+i] = bl
+		}
+		t++
+		b--
+		l++
+		r--
 	}
 }
