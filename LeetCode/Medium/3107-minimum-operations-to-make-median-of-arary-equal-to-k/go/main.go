6348df87f7cfffb768f7861a6c904f72c2bd6da1
rf <rf@ryans-MacBook-Air.local>
2024-04-08 08:04:36 +1000

    leetcode 3107-minimum-operations-to-make-median-of-arary-equal-to-k

--- LeetCode/Medium/3107-minimum-operations-to-make-median-of-arary-equal-to-k/go/main.go
+++ LeetCode/Medium/3107-minimum-operations-to-make-median-of-arary-equal-to-k/go/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+	"sort"
+)
 
+func main() {
+	fmt.Println(minOperationsToMakeMedianK([]int{2, 5, 6, 8, 5}, 4))
+	fmt.Println(minOperationsToMakeMedianK([]int{2, 5, 6, 8, 5}, 7))
+	fmt.Println(minOperationsToMakeMedianK([]int{1, 2, 3, 4, 5, 6}, 4))
+}
+
+// NOTE: this questions assumes the median is just len(nums)/2
+/*
+* For all elements before mid, we ensure that those are at most k.
+For all elements after mid, we esure that those are at least k.
+The element at mid is our "median", so we ensure that it is exactly k.
+*/
+func minOperationsToMakeMedianK(nums []int, k int) int64 {
+	sort.Ints(nums)
+
+	mid := len(nums) / 2
+	var operations int
+
+	for i := mid - 1; i >= 0 && nums[i] > k; i-- {
+		operations += nums[i] - k
+	}
+	for i := mid + 1; i < len(nums) && nums[i] < k; i++ {
+		operations += k - nums[i]
+	}
+
+	operations += int(math.Abs(float64(k - nums[mid])))
+
+	return int64(operations)
 }
