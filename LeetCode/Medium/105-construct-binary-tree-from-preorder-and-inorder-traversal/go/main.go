603068f830da2395b68ee714604e7150ade44a49
rf <rf@ryans-MacBook-Air.local>
2024-05-08 09:04:02 +1000

    LeetCode/Medium/105-construct-binary-tree-from-preorder-and-inorder-traversal/

--- LeetCode/Medium/105-construct-binary-tree-from-preorder-and-inorder-traversal/go/main.go
+++ LeetCode/Medium/105-construct-binary-tree-from-preorder-and-inorder-traversal/go/main.go
@@ -1,12 +1,40 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	tree := buildTree([]int{3, 9, 10, 11, 20, 15, 7}, []int{10, 9, 11, 3, 15, 20, 7})
+
+	fmt.Println(tree)
 }
 
 func buildTree(preorder []int, inorder []int) *TreeNode {
+	pos := make(map[int]int)
 
-	return &TreeNode{}
+	for i := range inorder {
+		pos[inorder[i]] = i
+	}
+
+	p := 0
+
+	var dfs func(l, r int) *TreeNode
+	dfs = func(l, r int) *TreeNode {
+		if l > r || p >= len(preorder) {
+			return nil
+		}
+
+		node := &TreeNode{preorder[p], nil, nil}
+
+		p++
+
+		node.Left = dfs(l, pos[node.Val]-1)
+		node.Right = dfs(pos[node.Val]+1, r)
+
+		return node
+	}
+
+	return dfs(0, len(preorder)-1)
 }
 
 type TreeNode struct {
