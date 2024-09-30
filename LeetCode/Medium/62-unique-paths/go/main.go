8278eedb4645ade57030f1bae01e443e0cdcd4df
rf <rf@ryans-MacBook-Air.local>
2024-10-01 08:49:29 +1000

    62-unique-paths leetcode go

---
+++ LeetCode/Medium/62-unique-paths/go/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(uniquePaths(3, 7))
+	fmt.Println(uniquePaths(3, 2))
+}
+
+func uniquePaths(m int, n int) int {
+	grid := make([][]int, 0, m)
+	for range m {
+		grid = append(grid, make([]int, n))
+	}
+	grid[0][0] = 1
+
+	for r := 0; r < m; r++ {
+		for c := 0; c < n; c++ {
+			if r > 0 {
+				grid[r][c] += grid[r-1][c]
+			}
+			if c > 0 {
+				grid[r][c] += grid[r][c-1]
+			}
+		}
+	}
+
+	return grid[m-1][n-1]
+}
+
+func uniquePathsOptimised(m int, n int) int {
+	row, col := 1, 1
+	for r := 1; r < m; r++ {
+		for c := 1; c < n; c++ {
+			col := row + col
+		}
+	}
+
+	return grid[m-1][n-1]
+}
