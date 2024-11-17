9612f52f113ce9f4fcd6943cada41128acbaad7a
rf <rf@ryans-MacBook-Air.local>
2024-11-17 14:25:54 +1100

    LeetCode/Medium/2-add-two-numbers/ golang

---
+++ LeetCode/Medium/2-add-two-numbers/go1/main.go
@@ -0,0 +1,38 @@
+package main
+
+func main() {
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	next := dummy
+	carry := 0
+	for l1 != nil || l2 != nil {
+		v1, v2 := 0, 0
+		if l1 != nil {
+			v1 = l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			v2 = l2.Val
+			l2 = l2.Next
+		}
+
+		sum := v1 + v2 + carry
+		carry = sum / 10
+
+		next.Next = &ListNode{sum % 10, nil}
+		next = next.Next
+	}
+
+	if carry > 0 {
+		next.Next = &ListNode{carry, nil}
+	}
+
+	return dummy.Next
+}
