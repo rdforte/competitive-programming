ec4a9f934110d8436bbc1aaf79153ca61fde8332
rf <rf@ryans-MacBook-Air.local>
2025-01-04 17:11:48 +1100

    add optimised version

--- LeetCode/Medium/1679-max-number-of-k-sum-pairs/go/main.go
+++ LeetCode/Medium/1679-max-number-of-k-sum-pairs/go/main.go
@@ -8,6 +8,10 @@ import (
 func main() {
 	fmt.Println(maxOperations([]int{1, 2, 3, 4}, 5))
 	fmt.Println(maxOperations([]int{3, 1, 3, 4, 3}, 6))
+
+	fmt.Println(maxOperationsOptimised2Pass([]int{1, 2, 3, 4}, 5))
+	fmt.Println(maxOperationsOptimised2Pass([]int{3, 1, 3, 4, 3}, 6))
+	fmt.Println(maxOperationsOptimised2Pass([]int{4, 4, 1, 3, 1, 3, 2, 2, 5, 5, 1, 5, 2, 1, 2, 3, 5, 4}, 2))
 }
 
 // BRUTE FORCE
@@ -32,6 +36,38 @@ func maxOperations(nums []int, k int) int {
 				break
 			}
 		}
+	}
+
+	return res
+}
+
+// Space O(n)
+// time O(n)
+// This is similar to 2Sum in the sense that we can see the numbers we have
+// and then find the compliment.
+func maxOperationsOptimised2Pass(nums []int, k int) int {
+	mp := make(map[int]int, len(nums))
+
+	for _, n := range nums {
+		mp[n]++
+	}
+
+	res := 0
+	for _, n := range nums {
+		need := k - n
+
+		cur, hasCur := mp[n]
+		comp, hasComp := mp[need]
+
+		if hasCur && hasComp && cur > 0 && comp > 0 {
+			if n == need && cur < 2 {
+				continue
+			}
+			mp[need]--
+			mp[n]--
+			res++
+		}
+
 	}
 
 	return res
