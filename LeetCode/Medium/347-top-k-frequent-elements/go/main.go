0ce38fe54e0bf3d1988d6eb77920c05ff0c18082
rf <rf@ryans-MacBook-Air.local>
2025-02-24 17:05:33 +1100

    top k

--- LeetCode/Medium/347-top-k-frequent-elements/go/main.go
+++ LeetCode/Medium/347-top-k-frequent-elements/go/main.go
@@ -11,42 +11,51 @@ func main() {
 	fmt.Println(topKFrequent([]int{1, 10, 7, 1, 1, 4, 2, 2, 3}, 4))
 }
 
+// n*log*k + n + k
+// n*log*k
 func topKFrequent(nums []int, k int) []int {
-	mp := make(map[int]int)
+	// n = len nums
+	// n time
+	elem := make(map[int]int)
 	for _, n := range nums {
-		mp[n]++
+		elem[n]++
 	}
 
-	h := &MinHeap{}
+	h := &IntHeap{}
 	heap.Init(h)
 
-	for key, val := range mp {
-		heap.Push(h, []int{val, key})
+	// n * log * k
+	for key, val := range elem {
+		heap.Push(h, []int{key, val})
+		if h.Len() > k {
+			heap.Pop(h)
+		}
 	}
 
-	res := []int{}
-
-	for i := 0; i < k && h.Len() > 0; i++ {
-		el := heap.Pop(h).([]int)
-		res = append(res, el[1])
+	// k
+	res := make([]int, k)
+	for i := k - 1; i >= 0; i-- {
+		res[i] = heap.Pop(h).([]int)[0]
 	}
 
 	return res
 }
 
-type MinHeap [][]int
+type IntHeap [][]int
 
-func (h *MinHeap) Len() int           { return len(*h) }
-func (h *MinHeap) Less(i, j int) bool { return (*h)[i][0] > (*h)[j][0] }
-func (h *MinHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+func (h IntHeap) Len() int { return len(h) }
 
-func (h *MinHeap) Push(x any) {
+func (h IntHeap) Less(i, j int) bool { return h[i][1] < h[j][1] }
+
+func (h IntHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x any) {
 	// Push and Pop use pointer receivers because they modify the slice's length,
 	// not just its contents.
 	*h = append(*h, x.([]int))
 }
 
-func (h *MinHeap) Pop() any {
+func (h *IntHeap) Pop() any {
 	old := *h
 	n := len(old)
 	x := old[n-1]
