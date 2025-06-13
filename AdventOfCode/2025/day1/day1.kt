e031028a3df8fd93c025b7c5a0fb89e23617f81e
rf <rf@ryans-MacBook-Air.local>
2025-06-14 09:24:33 +1000

    AOC day1 pt1

--- AdventOfCode/2025/day1/day1.kt
+++ AdventOfCode/2025/day1/day1.kt
@@ -1,11 +1,38 @@
 package day1
 
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+import kotlin.math.absoluteValue
+
+const val basePath = "AdventOfCode/2025/day1"
+const val isExample = false
+
 fun main() {
-    println("hello world 2222")
+    val (list1, list2) = getListsSorted()
+
+    var sum = 0
+    list1.forEachIndexed { idx, id -> sum += (id - list2[idx]).absoluteValue }
+
+    println("Answer: $sum")
+
 }
 
-class HelloWorld {
-    fun hello(): String {
-        return "hello"
+fun getListsSorted(): Pair<List<Int>, List<Int>> {
+    val list1 = mutableListOf<Int>()
+    val list2 = mutableListOf<Int>()
+
+    val lines = readInputLines(isExample)
+    lines.forEach {
+        val (id1, id2) = it.split(Regex("\\s+"))
+        list1 += id1.toInt()
+        list2 += id2.toInt()
     }
+
+    return Pair(list1.sorted(), list2.sorted())
+}
+
+fun readInputLines(isExample: Boolean): List<String> {
+    val fileName = if (isExample) "$basePath/inputExample.txt" else "$basePath/input.txt"
+    val path = Path(fileName)
+    return path.readLines()
 }
--- AdventOfCode/build.gradle.kts
+++ AdventOfCode/build.gradle.kts
@@ -2,25 +2,12 @@ plugins {
     kotlin("jvm") version "2.1.21"
 }
 
-//group = "com.github.rdforte"
-
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 sourceSets {
     main {
         kotlin.srcDirs("2025") // Add multiple advent of code src dirs here.
-    }
-    test {
-        kotlin.srcDirs("test2025")
     }
 }
