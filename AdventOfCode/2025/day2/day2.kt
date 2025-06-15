f7f1f5da141adc5de18b830aec4dd7e19204c4b3
rf <rf@ryans-MacBook-Air.local>
2025-06-16 09:41:54 +1000

    aoc day2 part 2

--- AdventOfCode/2025/day2/day2.kt
+++ AdventOfCode/2025/day2/day2.kt
@@ -7,8 +7,47 @@ import kotlin.math.absoluteValue
 const val basePath = "AdventOfCode/2025/day2"
 const val isExample = false
 
+/*
+* LEARNINGS:
+* 5 scope level functions - let, run, with, apply, also.
+* scope level function takes an Extension Lambda which gives us access to the receiver.
+*
+* mutable lists have a method called removeAt which allows us to easily remove the element at that index.
+* */
+
 fun main() {
     partOne()
+    partTwo()
+}
+
+fun partTwo() {
+    val reports = readInputLines().map { it.split(" ").map { level -> level.toInt() } }
+
+    var numSafeReports = 0
+
+    for (report in reports) {
+        if (isValidReport2(report)) {
+            numSafeReports++
+        }
+    }
+
+    println("Answer Part Two: $numSafeReports")
+}
+
+// Brute force by removing 1 element at a time and running isValidReport.
+// The input is not that large so this works and keeps the code relatively simple.
+fun isValidReport2(report: List<Int>): Boolean {
+    if (isValidReport(report)) {
+        return true
+    }
+
+    for (i in 0..report.lastIndex) {
+        if (isValidReport(report.toMutableList().apply { removeAt(i) })) { // use scope level function apply
+            return true
+        }
+    }
+
+    return false
 }
 
 fun partOne() {
