339ce78e07a34aa90e1c0e5bcc2627f5e243e1d7
rf <rf@ryans-MacBook-Air.local>
2025-06-20 09:09:30 +1000

    aoc day 3 refactor

--- AdventOfCode/2025/day3/day3.kt
+++ AdventOfCode/2025/day3/day3.kt
@@ -4,12 +4,21 @@ import utils.readInputLines
 
 const val isExample = false
 const val day = 3
+
 const val dontMul = "don't()"
 const val doMul = "do()"
 
+const val mulRgx = """mul\((\d+),(\d+)\)"""
+const val dontRgx = """don't\(\)"""
+const val doRgx = """do\(\)"""
+
 /*
 * LEARNINGS:
-* fold is a nice way to multiple all elements.
+* fold to multiple all elements.
+* Regex capturing groups for getting elements you need from a string.
+* extension functions make for cleaner / more readable code.
+* using """some-rgx""" triple quotes to avoid double \\ for rgx - String Literal.
+* converting string to Regex with .toRegex().
 * */
 
 fun main() {
@@ -19,18 +28,17 @@ fun main() {
 
 
 fun partTwo() {
-    val rgx = Regex("mul\\(\\d+,\\d+\\)|don't\\(\\)|do\\(\\)")
     val line = readInputLines(day, isExample)
 
     var shouldInclude = true
     var res = 0
     line.forEach { line ->
-        rgx.findAll(line).forEach {
+        "$mulRgx|$dontRgx|$doRgx".toRegex().findAll(line).forEach {
             when (it.value) {
                 doMul -> shouldInclude = true
                 dontMul -> shouldInclude = false
                 else -> if (shouldInclude) {
-                    res += getMultipliedVal(it.value)
+                    res += it.multiplyNumbers()
                 }
             }
         }
@@ -39,24 +47,20 @@ fun partTwo() {
     println("Answer Part Two: $res")
 }
 
-fun getMultipliedVal(str: String): Int {
-    val digitRgx = Regex("\\d+,\\d+")
-    return digitRgx.find(str)?.value?.split(",")?.fold(1) { acc, num -> acc * num.toInt() } ?: 0
-}
-
 fun partOne() {
-    val rgx = Regex("mul\\(\\d+,\\d+\\)")
     val line = readInputLines(day, isExample)
 
-    val digitRgx = Regex("\\d+,\\d+")
-
     var res = 0
-    line.forEach {
-        res += rgx.findAll(it)
-            .toMutableList().map { mul ->
-                digitRgx.findAll(mul.value).flatMap { nums -> nums.value.split(",") }.toMutableList()
-            }.sumOf { nums -> nums[0].toInt() * nums[1].toInt() }
+    line.forEach { line ->
+        res += mulRgx.toRegex().findAll(line).sumOf {
+            it.multiplyNumbers()
+        }
     }
 
     println("Answer Part One: $res")
+}
+
+private fun MatchResult.multiplyNumbers(): Int {
+    val (first, second) = this.destructured
+    return first.toInt() * second.toInt()
 }
